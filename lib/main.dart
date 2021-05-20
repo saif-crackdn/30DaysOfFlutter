f7a27eb7d4ed5c9c85ada10f43106a4c029d8302
Saif <saifhasan000786@gmail.com>
2021-05-20 13:02:11 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
+// material library provides us all the basic features like colours and all to build any app.  c
 void main() {
   runApp(MyApp());
 }
@@ -8,9 +10,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
   @override
+  //everything in the flutter is a widget. about 90% of things are widget.
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      //here we are declaring theme of the app
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => HomePage(),
+        "/Login": (context) => LoginPage()
+      },
     );
   }
 }
